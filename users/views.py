9ff57b53207211be1f957f20554a288a6d89016c
younusraza909 <younusraza909@gmail.com>
2020-05-08 11:19:29 -0700

    Login And Logout Pages using Django Inbuilt Views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -10,8 +11,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account Created For {username}! ")
-            return redirect("blog-home")
+            messages.success(
+                request, f"Account Created Has been Created You are Now Able To LogIn ! ")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
