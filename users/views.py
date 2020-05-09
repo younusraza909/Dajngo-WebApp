11d4a5c23aa9802a96c4ef743d5223d4f60a1aff
younusraza909 <younusraza909@gmail.com>
2020-05-09 03:31:11 -0700

    Updating Profile and resizing image

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, profileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -21,4 +21,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = profileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile)
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+            messages.success(request, "You Acount Has Been Updated")
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = profileUpdateForm(instance=request.user.profile)
+    context = {
+        "u_form": u_form,
+        "p_form": p_form
+    }
+    return render(request, "users/profile.html", context)
