d96d6d6bc344a36456c2440e16e389f4c9a8351c
younusraza909 <younusraza909@gmail.com>
2020-05-10 03:59:48 -0700

    Pagination useing paginator object

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView , UserPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
@@ -9,4 +9,5 @@ urlpatterns = [
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("about/", views.about, name="blog-about"),
     path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete"),
+    path('user/<str:username>',UserPostListView.as_view(),name="user-posts")
 ]
